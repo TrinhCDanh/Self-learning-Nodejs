ba4c505f8a21420e8a408ac8d1cca27a00b84bb9
TrinhCDanh <trinhdanh04@gmail.com>
2018-02-27 00:08:02 +0700

    params express

--- 08-url/server-express.js
+++ 08-url/server-express.js
@@ -8,4 +8,9 @@ app.get('/', (req, res) => {
     fs.createReadStream(__dirname + "/index.html", 'utf8').pipe(res);
 });
 
+// params
+app.get('/contact/:userid/send/:content', (req, res) => {
+    res.send(`This id user ${req.params.userid} with content ${req.params.content}`);
+});
+
 app.listen(8080);
